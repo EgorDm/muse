3f3a38db418493eaa1bbdb58ec4e738b231b52f1
Egor Dmitriev <egordmitriev2@gmail.com>
2017-07-23 01:13:55 +0200

    Fixed sample: load checkpoint and lowercase

--- sample.py
+++ sample.py
@@ -4,12 +4,13 @@ from trainers.MainTrainer import MainTrainer
 from utils import utils, config
 from utils.Logger import print_generated_text
 import argparse
+import tensorflow as tf
 
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--config', type=str, help='Config you want to load defaults from')
-    parser.add_argument('--checkpoint', type=str, help='Checkpoint you want to use')
+    parser.add_argument('--config', type=str, required=True, help='Config you want to load defaults from')
+    parser.add_argument('--checkpoint', type=str, required=True, help='Checkpoint you want to use')
     parser.add_argument('--prime', type=str, default='The ', help='Text to use to generate more text')
     parser.add_argument('--length', type=int, default=1000, help='Length of the generated text')
     settings = parser.parse_args()
@@ -19,13 +20,21 @@ def main():
 def run(settings):
     myconfig = config.load_config(settings.config)
     if myconfig is None: return
+    if myconfig.lc:
+        settings.prime = settings.prime.lower()
 
     batcher = CharacterBatcher([], myconfig.lc, False, myconfig.batch_size,
                                myconfig.seq_length)
     model = RNNModel(batcher, myconfig.nlayers, myconfig.cell_size, cell_type=utils.get_cell_type(myconfig.cell))
+
     trainer = MainTrainer(batcher, None, model, myconfig)
-    text = trainer.generate_text(settings.prime, settings.length)
-    print_generated_text(text)
+
+    with tf.Session() as sess:
+        trainer._sess = sess
+        load = tf.train.import_meta_graph('{}/{}.meta'.format(myconfig.save_dir, settings.checkpoint))
+        load.restore(sess, '{}/{}'.format(myconfig.save_dir, settings.checkpoint))
+        text = trainer.generate_text(settings.prime, settings.length)
+        print_generated_text(text)
 
 
 if __name__ == '__main__':
