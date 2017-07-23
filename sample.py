573c59c6677f752d0419d8ec73fd60272bf0a941
Egor Dmitriev <egordmitriev2@gmail.com>
2017-07-23 22:21:02 +0200

    Added save sample

--- sample.py
+++ sample.py
@@ -4,12 +4,15 @@ from trainers.MainTrainer import MainTrainer
 from utils import utils, config
 from utils.Logger import print_generated_text
 import argparse
+import math, time
 
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--config', type=str, help='Config you want to load defaults from')
     parser.add_argument('--checkpoint', type=str, help='Checkpoint you want to use')
+    parser.add_argument('--save', default='last_model', type=str, help='Name of file to save text to.')
+    parser.add_argument('--save_dir', default='data/save', type=str, help='Name of file to save text to.')
     parser.add_argument('--prime', type=str, default='The ', help='Text to use to generate more text')
     parser.add_argument('--length', type=int, default=1000, help='Length of the generated text')
     settings = parser.parse_args()
@@ -19,6 +22,7 @@ def main():
 def run(settings):
     myconfig = config.load_config(settings.config)
     if myconfig is None: return
+    timestamp = str(math.trunc(time.time()))
 
     batcher = CharacterBatcher([], myconfig.lc, False, myconfig.batch_size,
                                myconfig.seq_length)
@@ -26,6 +30,11 @@ def run(settings):
     trainer = MainTrainer(batcher, None, model, myconfig)
     text = trainer.generate_text(settings.prime, settings.length)
     print_generated_text(text)
+
+    utils.mkdir_or_not(settings.save_dir)
+    save_path = '{}/{}_{}.txt'.format(settings.save_dir, settings.save, timestamp)
+    with open(save_path, 'w') as f:
+        f.write(text)
 
 
 if __name__ == '__main__':
